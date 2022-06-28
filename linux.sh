5956376e08b33772b378b6791d9e9b6acf7666f1
iksior <iksior1@gmail.com>
2022-06-28 23:53:02 +0200

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -16,3 +16,5 @@ ssh-keygen -t ed25519 -C "nazwa_klucza"
 #-----------------------ANSIBLE------------------------
 #upgrade all hosts from inventory using ansible.cnf from ansible repo
 ansible all -m apt -a "upgrade=dist" --become --ask-become-pass
+#jak puscic playbook?
+ansible-playbook --ask-become-pass install_htop.yml
