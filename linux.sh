63e6f1de525c1765d4193a0af06ae04c2ef0a616
iksior <iksior1@gmail.com>
2022-06-28 15:39:39 +0200

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,10 +1,15 @@
 
 # my_cheat_sheet
 #LINUX
+
+#-----------------------SSH------------------------
+#--
 #Jeśli mamy klucz ssh z hasłem możemy puścić tak
 #logowanie #logowanieBezHasła #linux #hasło #password 
 eval $(ssh-agent)
 ssh-add (wpisz hasło)
 #-||-
 alias ssha='eval $(ssh-agent) && ssh-add'
-
+#--
+#generowanie klucza
+ssh-keygen -t ed25519 -C "nazwa_klucza"
